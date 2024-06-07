5031091d27026aee6d8eefe3df54bbce2bdaf60f
LONGMANKE <mburunjoroge0@gmail.com>
2024-06-07 11:37:25 +0300

    Onchange Functions

--- custom_modules/hospital_management/models/patients.py
+++ custom_modules/hospital_management/models/patients.py
@@ -1,7 +1,8 @@
 from odoo import api, models, fields, _
 
+
 class HospitalPatient(models.Model):
-    _name ="hospital.patient"
+    _name = "hospital.patient"
     _inherit = 'mail.thread'
     _description = 'Hospital Patient'
 
@@ -9,10 +10,13 @@ class HospitalPatient(models.Model):
     age = fields.Integer(string='Age', tracking=True)
     is_child = fields.Boolean(string="Is Child ?", tracking=True)
     notes = fields.Text(string='Notes', tracking=True)
-    gender = fields.Selection([('male', 'Male'),('female', 'Female'), ('others', 'Others')], string='Gender', tracking=True)
-
-
-
-
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender',
+                              tracking=True)
+    @api.onchange('age')
+    def _onchange_age(self):
+        if self.age <= 10:
+            self.is_child = True
+        else:
+            self.is_child = False
 
 
