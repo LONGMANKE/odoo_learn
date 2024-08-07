d0cc61a1111847ee2cb2e379bc6c9390ee02756b
LONGMANKE <mburunjoroge0@gmail.com>
2024-08-07 17:47:57 +0300

    code sequence

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -1,11 +1,21 @@
 from odoo import api, fields, models
 
+
 class hospitalAppointment(models.Model):
-      _name = "hospital.appointment"
-      _inherit = ['mail.thread']
-      _description= 'Hospital Appointment'
+    _name = "hospital.appointment"
+    _inherit = ['mail.thread']
+    _description = 'Hospital Appointment'
+    _rec_name = "patient_id"
+
+    reference = fields.Char(string="Reference", default='Appointment Code')
+    patient_id = fields.Many2one('hospital.patient', string="Patient")
+    date_appointment = fields.Date(string="Date", tracking=True)
+    note = fields.Text(string="Note")
 
 
-      patient_id = fields.Many2one('hospital.patient', string="Patient")
-      date_appointment = fields.Date(string="Date", tracking= True)
-      note = fields.Text(string="Note")
+    @api.model_create_multi
+    def create(self, vals_list):
+      for vals in vals_list:
+         if not vals.get('reference') or vals['references'] == 'New':
+                  vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
+      return super().create(vals_list)
