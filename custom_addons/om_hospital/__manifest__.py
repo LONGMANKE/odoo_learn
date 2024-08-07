9cbf63f423ba1a769a9a31c80114a479120c28bb
LONGMANKE <mburunjoroge0@gmail.com>
2024-08-07 15:26:59 +0300

    progress, added the menu items as per tutorial

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -2,5 +2,10 @@
     "name" : "Hospital Management System",
     "author" : "Simon Mburu",
     "license" : "LGPL-3",
-    "version" : "17.0.1.1"
+    "version" : "17.0.1.1",
+    "data" : [
+        "security/ir.model.access.csv",
+        "views/patient_view.xml",
+        "views/menu.xml",
+    ]
 }
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -7,3 +7,4 @@ class hospitalPatient(models.Model):
 
       name = fields.Char(string="Name", required=True)
       date_of_birth = fields.Date(string="DOB")
+      gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
