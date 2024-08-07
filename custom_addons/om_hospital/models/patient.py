ce779ef8c97e1c90f0ba70beb0cee1d09c986e7d
LONGMANKE <mburunjoroge0@gmail.com>
2024-08-07 16:27:29 +0300

    tracking the fields in models

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -6,6 +6,6 @@ class hospitalPatient(models.Model):
       _description= 'Patient Master'
 
 
-      name = fields.Char(string="Name", required=True)
-      date_of_birth = fields.Date(string="DOB")
+      name = fields.Char(string="Name", required=True, tracking= True)
+      date_of_birth = fields.Date(string="DOB", tracking= True)
       gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
