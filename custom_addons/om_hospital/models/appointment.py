683342df97374039cf5f574f3d30c4f43009dcfc
LONGMANKE <mburunjoroge0@gmail.com>
2024-08-14 15:16:59 +0300

    adding states for the appointments

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -11,6 +11,9 @@ class hospitalAppointment(models.Model):
     patient_id = fields.Many2one('hospital.patient', string="Patient")
     date_appointment = fields.Date(string="Date", tracking=True)
     note = fields.Text(string="Note")
+    state =  fields.Selection([('draft', 'Draft'), ('confirmed', 'Confirmed'),('ongoing', 'Ongoing'),
+                               ('done', 'Done'),('cancelled', 'Cancelled')
+                               ], string="Gender")
 
 
     @api.model_create_multi
