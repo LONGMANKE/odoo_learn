12eab9679ae5be496336b41939778f4c69fe7595
LONGMANKE <mburunjoroge0@gmail.com>
2024-11-25 16:07:59 +0300

    Boolean field

--- custom_addons/student/models/models.py
+++ custom_addons/student/models/models.py
@@ -7,6 +7,7 @@ class Student(models.Model):
     _name = 'wb.student'
     _description = 'This is a student profile'
 
+    is_paid = fields.Boolean(string="Paid?", default=True, help="This is the field for paid")
     name = fields.Char("Name")
     name1 = fields.Char("Name1")
     name2 = fields.Char("Name2", copy=False)
