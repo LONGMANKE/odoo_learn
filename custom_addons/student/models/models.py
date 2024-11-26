a3bac9d545adf4c2a2da41084269b7d2fca80ed4
LONGMANKE <mburunjoroge0@gmail.com>
2024-11-26 08:16:56 +0300

    Creating a Selection Field (Combobox)

--- custom_addons/student/models/models.py
+++ custom_addons/student/models/models.py
@@ -7,14 +7,35 @@ class Student(models.Model):
     _name = 'wb.student'
     _description = 'This is a student profile'
 
+    @api.model
+    def _get_vip_list(self):
+        return [('a', '1'), ('b', '2'), ('c', '3')]
+
+    gender = fields.Selection(
+        [('male', 'Male'), ('female', 'Female')], required=1
+    )
+    advanced_gender = fields.Selection("_get_advanced_gender_list")
+    vip_gender = fields.Selection(_get_vip_list, "VIP Gen")
+
+    combobox = fields.Selection(selection=[('male', 'Male'), ('female', 'Female')],
+                                string="Combo Box")
+
+    is_default_demo = fields.Boolean(default=True, required=1)
     is_paid = fields.Boolean(string="Paid?", default=True, help="This is the field for paid")
+
     name = fields.Char("Name")
     name1 = fields.Char("Name1")
     name2 = fields.Char("Name2", copy=False)
     name3 = fields.Char("Name3", default="learn")
     name4 = fields.Char("Name4", readonly=True)
 
-    student_name= fields.Char(string="Student", required=True, index=True, size=10)
-    address = fields.Text("Student Address", required=True, help="Enter here student address", default="Student address")
+    student_name = fields.Char(string="Student", required=True, index=True, size=10)
+    address = fields.Text("Student Address", required=True, help="Enter here student address",
+                          default="Student address")
 
-    address_html= fields.Html("Address Html", help="This field is use for the dynamic html code to render into the student profile.", copy=False)
+    address_html = fields.Html("Address Html",
+                               help="This field is use for the dynamic html code to render into the student profile.",
+                               copy=False)
+
+    def _get_advanced_gender_list(self):
+        return [('male', 'Male'), ('female', 'Female')]
