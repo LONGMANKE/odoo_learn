90e0b103a00b487394550e29d535d10315eaff08
LONGMANKE <mburunjoroge0@gmail.com>
2025-05-05 12:11:59 +0300

    how to override the write method

--- custom_addons/student/models/models.py
+++ custom_addons/student/models/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
-import time
 
 
 class School(models.Model):
@@ -11,30 +10,31 @@ class School(models.Model):
     image = fields.Image("School Image", max_width=128, max_height=128)
     name = fields.Char("School Name")
     invoice_id = fields.Many2one("account.move")
-    #this will be a virtual field
-    invoice_user_id= fields.Many2one("res.users",  related="invoice_id.invoice_user_id")
-    #This will be stored in the table //brings also like the search by sales person in  the group by
+    # this will be a virtual field
+    invoice_user_id = fields.Many2one("res.users", related="invoice_id.invoice_user_id")
+    # This will be stored in the table //brings also like the search by sales person in  the group by
     # invoice_user_id= fields.Many2one("res.users",  related="invoice_id.invoice_user_id", storable=True)
-    invoice_date= fields.Date(related="invoice_id.invoice_date")
+    invoice_date = fields.Date(related="invoice_id.invoice_date")
     student_list = fields.One2many("wb.student", "school_id", string="Students", readonly=1,
                                    help="This field is used to display related students list for this current school.")
 
-    ref_field_id = fields.Reference(selection=[('wb.school','School'),
-                                     ('wb.student','Student'),
-                                     ('wb.hobby','Hobby'),
-                                     ('sales.order','Sale'),
-                                     ('account.move','Invoice'),
-                                     ('purchase.order','Purchase'),
-                                     ], string="reference", required=1, help="Please select records accordingly") #we can use default
+    ref_field_id = fields.Reference(selection=[('wb.school', 'School'),
+                                               ('wb.student', 'Student'),
+                                               ('wb.hobby', 'Hobby'),
+                                               ('sales.order', 'Sale'),
+                                               ('account.move', 'Invoice'),
+                                               ('purchase.order', 'Purchase'),
+                                               ], string="reference", required=1,
+                                    help="Please select records accordingly")  # we can use default
     binary_field = fields.Binary(string="Upload file", copy=False)
     binary_file_name = fields.Char("Binary Field Name")
     binary_fields = fields.Many2many("ir.attachment", string="Multi Files Upload")
-    #this currency_id is known by default
+    # this currency_id is known by default
     # currency_id = fields.Many2one("res.currency", "Currency")
     # amount = fields.Monetary("Amount")
 
-    #use this if like you name the field anything else rather than currency_id
-    my_currency_id = fields.Many2one("res.currency", string="(My Currency)" )
+    # use this if like you name the field anything else rather than currency_id
+    my_currency_id = fields.Many2one("res.currency", string="(My Currency)")
     amount = fields.Monetary("Amount", currency_field="my_currency_id")
 
     # def create(self, vals):
@@ -58,11 +58,44 @@ class School(models.Model):
         rtn = super().create(vals)
         print(rtn)
         return rtn
+
+    def custom_method(self):
+        print("Custom method clicked")
+        print(self)
+        # Method 1
+        # self.name = "Single update"
+        # self.amount = 5000
+
+        # Method 2 this runs like individually
+        # self.update({"name": "Write update", "amount": 50})
+        # Method 3 make them run at once // single write ,method
+        # self.write({"name": "Write update", "amount": 50})
+
+        # Mass editing
+        # records = self.search([], limit=5)
+        # print(records)
+
+        # records.write({"name": "Mass Editing", "amount": 1000})
+
+        # Alternative for above
+        # for rec in records:
+        #     rec.write({"name": f"{rec.id}", "amount":2000})
+        pass
+
+    def write(self, vals):
+        print("Write method called")
+        print(self)
+        print(vals)
+        rtn = super(School, self).write(vals)
+        print(rtn)
+        return rtn
+
+
 class Student(models.Model):
     _name = 'wb.student'
     _description = 'This is a student profile'
 
-    hobby_list = fields.Many2many("wb.hobby", "student_hobby_list_relation","student_id","hobby_id")
+    hobby_list = fields.Many2many("wb.hobby", "student_hobby_list_relation", "student_id", "hobby_id")
     hobby_list_ids = fields.Many2many("wb.hobby"
                                       )
     # school_id = fields.Many2one("wb.school")
@@ -126,11 +159,12 @@ class Student(models.Model):
         for record in self:
             record.compute_address_html = record.address_html
 
-    @api.onchange("student_fees","discount_fees")
+    @api.onchange("student_fees", "discount_fees")
     # @api.depends("student_fees","discount_fees")
     def _compute_final_fees_cal(self):
         for record in self:
             record.final_fees = record.student_fees - record.discount_fees
+
     def _get_advanced_gender_list(self):
         return [('male', 'Male'), ('female', 'Female')]
 
